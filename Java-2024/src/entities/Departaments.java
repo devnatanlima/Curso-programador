772b8065f35e2ab0d2d38c6dcf037b404c27b460
Natanael Nunes <natanaelnunes@MacBook-Pro-de-Natanael.local>
2024-01-25 19:55:14 -0300

    atualizando os itens que estavam dando erro novamente

---
+++ Java-2024/src/entities/Departaments.java
@@ -0,0 +1,22 @@
+package entities;
+
+public class Departaments {
+          private String name;
+
+          public Departaments(){
+
+          }
+
+          public Departaments(String name) {
+                    this.name = name;
+          }
+
+          public String getName() {
+                    return name;
+          }
+
+          public void setName(String name) {
+                    this.name = name;
+          }
+          
+}
