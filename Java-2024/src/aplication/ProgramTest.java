f93632f57c9d9e6e8a82b8ad4f0abcca38566d59
natanael <natan.pvc@gmail.com>
2024-02-22 12:24:26 -0300

    Java 22 de fev

---
+++ Java-2024/src/aplication/ProgramTest.java
@@ -0,0 +1,19 @@
+import entities.Account;
+import entities.BusinessAccount;
+import entities.SavingsAccount;
+
+public class ProgramTest {
+    public static void main(String[] args) {
+        Account acc1 = new Account(101, "natan", 50000.0);
+        acc1.withdraw(200.0);
+        System.out.println("R$ " + acc1.getBalance() );
+
+        Account acc2 = new SavingsAccount(1002, "Maria", 1000.0, 0.01);
+        acc2.withdraw(200.0);
+        System.out.println(acc2.getBalance());
+
+        Account acc3 = new BusinessAccount(1003, "Bob", 1000.0, 500.0);
+        acc3.withdraw(200.0);
+        System.out.println(acc3.getBalance());
+    }
+}
--- Java-2024/src/entities/BusinessAccount.java
+++ Java-2024/src/entities/BusinessAccount.java
@@ -2,9 +2,9 @@ package entities;
 
 public class BusinessAccount extends Account {
     private Double loanLimit;
-    
-    public BusinessAccount (){
-super();
+
+    public BusinessAccount() {
+        super();
     }
 
     public BusinessAccount(Integer number, String holder, Double balance, Double loanLimit) {
@@ -20,11 +20,16 @@ super();
         this.loanLimit = loanLimit;
     }
 
-    public void loan (double amount){
-        if(amount <= loanLimit) {
-        balance += amount - 10.0;
-     }
+    public void loan(double amount) {
+        if (amount <= loanLimit) {
+            balance += amount - 10.0;
+        }
     }
-  
-    
+
+    @Override
+    public void withdraw(double amount) {
+        super.withdraw(amount);
+        balance -= 2.0;
+    }
+
 }
